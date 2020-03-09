1f487c1bea8b840d3e0ab6e1193ead7b69bdb10e
GoranJakovleski <zdravotoner@gmail.com>
2020-03-09 21:16:29 +0100

    homework01

---
+++ trafficLight/trafficLight/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace trafficLight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("STOP");
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("REDY");
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("GO");
+
+            Console.ResetColor();
+
+            Console.ReadLine();
+        }
+    }
+}
